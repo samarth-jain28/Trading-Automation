659f64a0af6966d6a8ecb9487130c6b3f9adaf10
Samarth Jain <samarthjain28.2002@gmail.com>
2023-10-03 13:38:55 +0530

    created base.html

--- Application/routes.py
+++ Application/routes.py
@@ -1,5 +1,6 @@
 from Application import app
+from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world"
+    return render_template("base.html")
