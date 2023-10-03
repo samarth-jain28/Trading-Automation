0b4f2e791bedfe3fb16721c21ebff425728a885f
Samarth Jain <samarthjain28.2002@gmail.com>
2023-10-03 23:31:17 +0530

    added welcome page

--- Application/routes.py
+++ Application/routes.py
@@ -1,6 +1,12 @@
 from Application import app
 from flask import render_template
+
 @app.route("/")
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+
 @app.route("/index")
 def index():
     return render_template("base.html")
