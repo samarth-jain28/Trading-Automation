537c091647ee66451cba12e0b51c6fa7c9212219
Samarth Jain <samarthjain28.2002@gmail.com>
2023-10-07 22:56:57 +0530

    Added login.html file

--- Application/routes.py
+++ Application/routes.py
@@ -14,3 +14,7 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html" , title="About Us")
+
+@app.route("/login")
+def login():
+    return render_template("login.html" , title="Login")
